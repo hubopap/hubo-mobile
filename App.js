8f3ddc381b826c2a1ba32c680bc859a95d5d416b
root <root@DESKTOP-E1O55OU.localdomain>
2023-02-18 13:56:20 +0000

    primeiro commit - pagina de home e começo de layout

--- App.js
+++ App.js
@@ -1,20 +1,50 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { TouchableOpacity, StyleSheet, Image, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+  render(){
+    return(
+      <View style={styles.container}>
+        <Image 
+          source={require('./assets/logo_hubo_square.png')}
+          style={styles.logo}
+          resizeMode="contain"
+        ></Image>
+        <Text style={styles.text}>Work Smart, Work Hubo.</Text>
+        <TouchableOpacity>
+            <Text style={styles.get_started}>Get Started</Text>
+          </TouchableOpacity>
+      </View>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
   },
+  text: {
+    color: 'black',
+    marginTop: '5%',
+    fontSize: 20
+  },
+  logo:{
+    width: 280,
+    height: 280,
+    margin: 'auto',
+    marginTop: '25%'
+  },
+  get_started: {
+    backgroundColor: '#3A59FF',
+    color: 'white',
+    width: "75%",
+    borderRadius: 30,
+    textAlign: 'center',
+    fontWeight: 'bold',
+    padding: "3%",
+    fontSize:  20,
+    marginTop: '40%'
+  }
 });
