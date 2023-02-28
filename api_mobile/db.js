3c5ecd00adc75148402a1e56bc0794e4db86cd04
nunoepteixeira <nunoepteixeira@outlook.pt>
2023-02-28 23:27:20 +0000

    mobile api

--- api_mobile/db.js
+++ api_mobile/db.js
@@ -1,6 +1,6 @@
 const Sequelize =  require('sequelize');
 
-const sequelize = new Sequelize('hubo', 'root', '', {
+const sequelize = new Sequelize('hubo', 'admin', 'admin123', {
   host: 'localhost',
   dialect: 'mariadb',
   define: {
