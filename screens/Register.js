3db373ec8521d6f28f254dd5a59636f0a7a7bbff
nunoepteixeira <nunoepteixeira@outlook.pt>
2023-03-01 02:47:11 +0000

    presentation ready

--- screens/Register.js
+++ screens/Register.js
@@ -1,94 +1,99 @@
-import React, {useState} from 'react';
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { TouchableOpacity, StyleSheet, Image, TextInput, Text, View,} from 'react-native';
+import DatePicker from 'react-native-datepicker';
 import axios from 'axios';
-
-export default function Register({navigation}){
-  
-  const [registerUsername, setRegisterUsername] = useState("");
-  const [registerPassword, setRegisterPassword] = useState("");
-  const [registerEmail, setRegisterEmail] = useState("");
-  const [data, setData] = useState(null);
+import { useNavigation } from '@react-navigation/native';
 
 
-  const register = () => {
-    axios({
-      method: "post",
-      data: {
-        username: registerUsername,
-        password: registerPassword,
-        email: registerEmail
-      },
-      withCredentials: true,
-      url: "http://hubo.pt:3001/register"
-    }).then((res) => setData(res.data));
+export default class Register extends React.Component{
+
+    state = {
+      username: '', password: '', email: '', phone_number: ''
+    }
+
+  onChangeText = (key, val) => {
+    this.setState({ [key]: val })
   }
 
-  const login = () => {
-    axios({
-      method: "post",
-      data: {
-        username: registerUsername,
-        password: registerPassword
-      },
-      withCredentials: true,
-      url: "http://hubo.pt:3001/login"
-    }).then((res) => console.log(res));
-  }
-
-  const getUser = () =>  {
-    axios({
-      method: "GET",
-      withCredentials: true,
-      url: "http://hubo.pt:3001/user"
-    }).then((res) => console.log(res));
-  }
-
-  return (
-    <View style={styles.container}>
+  render(){
+    const current = new Date();
+    return(
+      <View style={styles.container}>
+        <Image 
+          source={require('../assets/logo_hubo_square.png')}
+          style={styles.logo}
+          resizeMode="contain"
+        ></Image>
         <TextInput
-          value={registerUsername}
-          placeholder="Username"
-          editable
-          maxLength={20}
-          onChangeText={(registerUsername) => setRegisterUsername(registerUsername)}
+          style={styles.input}
+          placeholder='Username'
+          autoCapitalize="none"
+          placeholderTextColor='white'
+          onChangeText={val => this.onChangeText('username', val)}
         />
         <TextInput
-          placeholder="Password"
-          value={registerPassword}
-          editable
-          maxLength={20}
+          style={styles.input}
+          placeholder='Password'
           secureTextEntry={true}
-          onChangeText={(registerPassword) => setRegisterPassword(registerPassword)}
+          autoCapitalize="none"
+          placeholderTextColor='white'
+          onChangeText={val => this.onChangeText('password', val)}
         />
         <TextInput
-          value={registerEmail}
-          placeholder="Email"
-          editable
-          maxLength={50}
-          onChangeText={(registerEmail) => setRegisterEmail(registerEmail)}
+          style={styles.input}
+          placeholder='Email'
+          autoCapitalize="none"
+          placeholderTextColor='white'
+          onChangeText={val => this.onChangeText('email', val)}
         />
-        <TouchableOpacity onPress={register}>
-          <Text>Register</Text>
+        <TouchableOpacity>
+            <Text style={styles.register}>Register</Text>
         </TouchableOpacity>
-
-        <TouchableOpacity onPress={login}>
-          <Text>Login</Text>
-        </TouchableOpacity>
-
-        <TouchableOpacity onPress={getUser}>
-          <Text>getUser</Text>
-        </TouchableOpacity>
-    </View>
-  )
+      </View>
+    )
+  }
+  
 }
 
 const styles = StyleSheet.create({
+  input: {
+    marginTop: 10,
+    width: "85%",
+    height: 40,
+    backgroundColor: '#42A5F5',
+    margin: 10,
+    padding: 8,
+    color: 'white',
+    borderRadius: 25,
+    fontSize: 15,
+    fontWeight: '500',
+  },
   container: {
-    flex: 1,
-    flexDirection: 'column',
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: 'yellow',
+  },
+  text: {
+    color: 'black',
+    marginTop: '5%',
+    fontSize: 20
+  },
+  logo:{
+    width: 100,
+    height: 100,
+    margin: 'auto',
+    marginTop: '40%',
+    marginBottom: '20%',
+  },
+  register: {
+    backgroundColor: '#3A59FF',
+    color: 'white',
+    width: "75%",
+    borderRadius: 30,
+    textAlign: 'center',
+    fontWeight: 'bold',
+    padding: "3%",
+    fontSize:  20,
+    marginTop: '20%'
   }
-})
+});
