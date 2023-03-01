37734048661effb9528984612b50edbc2532c960
nunoepteixeira <nunoepteixeira@outlook.pt>
2023-03-01 02:23:38 +0000

    tentativa asjhdgahsjgdhjasgd

--- api/models/user.js
+++ api/models/user.js
@@ -1,28 +1,26 @@
 const db = require('../db');
-const Sequelize = db.Sequelize;
-const sequelize = db.sequelize;
 
-const User = sequelize.define('User', {
+const User = db.sequelize.define('User', {
     id_user:{
-        type: Sequelize.INTEGER,
+        type: db.Sequelize.INTEGER,
         allowNull: false,
         autoIncrement: true,
         primaryKey: true
     },
     username:{
-        type: Sequelize.STRING(45),
+        type: db.Sequelize.STRING(45),
         allowNull: false,
         unique: true
     },
     password:{
-        type: Sequelize.STRING(255),
+        type: db.Sequelize.STRING(255),
         allowNull: false
     },
     bio_user:{
-        type: Sequelize.STRING(255)
+        type: db.Sequelize.STRING(255)
     },
     email_user:{
-        type: Sequelize.STRING(255),
+        type: db.Sequelize.STRING(255),
          allowNull: false
     }
 });
